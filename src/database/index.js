349e35fad581936d3626d716536a9f63d7bd9b35
tiago <tiagoisrael77@hotmail.com>
2019-03-22 21:54:27 -0300

    troca de url mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:123@qwe@cluster0-go1k0.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
